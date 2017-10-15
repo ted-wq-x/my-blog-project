c39123c14c0782f8f8f1abbcb4bfbe1c42466911
BTED <blueted@outlook.com>
2017-10-15 14:32:00 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 #cd /path/to/your-project
 # git pull origin master
-mkdir fuck
+
+cd /usr/java/
+if [ ! -d "./core_proj_bak" ]; then
+  mkdir core_proj_bak
+fi
+# 备份文件
+temp=$(date +%Y-%m-%d-%H-%M-%S)
+filetemp=*.jar
+mv $filetemp core_proj_bak/$temp.jar
+
+mv ~/*.jar /usr/java/core-blog.jar
+rm -rf ~/*.jar
+
 echo 'travis build done!'
